11e15c0078bc2ce98cd77f50cad7a9eb701557ed
Scarface123 <Scarface123@users.noreply.github.com>
2017-03-18 20:11:17 -0400

    Files added

--- List.h
+++ List.h
@@ -17,9 +17,10 @@ class List //begin List definition
      ~List();//destructor
      void insert(int element, int k);//insert element at location k
      void remove(int k);//remove element at location k
+     int get(int k);
      int size();//return the number of elements in the List
-     
-     
+     void display(); // this makes the list empty
+     void clear();
      /** MISSING OPERATIONS */
      //... fill in ....
 
